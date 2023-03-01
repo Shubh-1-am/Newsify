119102f18876a96f18a9a57af6f7a96acb0b8e75
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-03-01 21:19:10 +0530

    Added API service class

--- Newsify/app/src/main/java/com/example/newsify/retrofit/api/ApiService.kt
+++ Newsify/app/src/main/java/com/example/newsify/retrofit/api/ApiService.kt
@@ -1,4 +1,38 @@
 package com.example.newsify.retrofit.api
 
-class ApiService {
+import com.example.newsify.retrofit.response.NewsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("v2/top-headlines")
+    suspend fun getNews(
+        @Query("country")
+        countryCode: String,
+        @Query("apiKey")
+        apiKey: String
+    ): NewsResponse
+
+    @GET("v2/top-headlines")
+    suspend fun getTopHeadlines(
+        @Query("country") country: String,
+        @Query("category") category: String,
+        @Query("page") page: Int,
+        @Query("pageSize") pageSize: Int,
+        @Query("apiKey") apiKey: String
+    ): NewsResponse
+
+
+
+    @GET("v2/everything")
+    suspend fun getSearchNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        page: Int = 1,
+        @Query("apiKey")
+        apiKey: String
+    ): NewsResponse
 }
