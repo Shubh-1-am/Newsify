08d444b28e094a444c341862a3b9a0f7fdfe04c4
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-03-01 21:18:47 +0530

    Added Paging Source class

--- Newsify/app/src/main/java/com/example/newsify/paging/NewsPagingSource.kt
+++ Newsify/app/src/main/java/com/example/newsify/paging/NewsPagingSource.kt
@@ -1,4 +1,34 @@
 package com.example.newsify.paging
 
-class NewsPagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.newsify.retrofit.api.ApiService
+import com.example.newsify.retrofit.response.Article
+import com.example.newsify.utils.Constants
+import retrofit2.HttpException
+
+const val STARTING_PAGE_INDEX = 1
+class NewsPagingSource (private val apiService: ApiService) : PagingSource<Int, Article> () {
+
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        val position = params.key ?: STARTING_PAGE_INDEX
+        return try {
+            val response = apiService.getTopHeadlines("us", "business", position, params.loadSize, Constants.API_KEY)
+            val articles = response.articles
+            LoadResult.Page(
+                data = articles,
+                prevKey = if (position == STARTING_PAGE_INDEX) null else position - 1,
+                nextKey = if (articles.isEmpty()) null else position + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        } catch (e: HttpException) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+        return state.anchorPosition
+    }
 }
