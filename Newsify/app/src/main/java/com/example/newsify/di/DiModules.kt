90b6a605027268a920a70633d7346a26f1c20fba
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-03-01 21:17:16 +0530

    Added DI module package for DI related files

--- Newsify/app/src/main/java/com/example/newsify/di/DiModules.kt
+++ Newsify/app/src/main/java/com/example/newsify/di/DiModules.kt
@@ -1,4 +1,32 @@
 package com.example.newsify.di
 
+import com.example.newsify.retrofit.api.ApiService
+import com.example.newsify.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 object DiModules {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit):ApiService{
+        return retrofit.create(ApiService::class.java)
+    }
 }
