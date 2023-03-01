2318fac17128c8526835ca6cb40459100e5a2329
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-03-01 21:44:44 +0530

    Added some Constants

--- Newsify/app/src/main/java/com/example/newsify/utils/Constants.kt
+++ Newsify/app/src/main/java/com/example/newsify/utils/Constants.kt
@@ -1,4 +1,7 @@
 package com.example.newsify.utils
 
 object Constants {
+
+    const val API_KEY : String = "8d5f1efed26449a2b7c92256e2f40d2e"
+    const val BASE_URL: String = "https://newsapi.org/"
 }
