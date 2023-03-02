6469e4a39b9b1d29eea3934fbda2a476b5657f74
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-03-02 21:38:51 +0530

    Added Binding Adapters

--- Newsify/app/src/main/java/com/example/newsify/binding/BindingAdapters.kt
+++ Newsify/app/src/main/java/com/example/newsify/binding/BindingAdapters.kt
@@ -1,4 +1,41 @@
 package com.example.newsify.binding
 
-class BindingAdapters {
+import android.annotation.SuppressLint
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.newsify.R
+import com.example.newsify.utils.formatTimeAgo
+import javax.inject.Inject
+
+class BindingAdapters @Inject constructor() {
+
+    companion object{
+        @BindingAdapter("urlToImage")
+        @JvmStatic
+        fun loadImage(view: ImageView, url: String?){
+            try {
+                Glide.with(view.context).setDefaultRequestOptions(RequestOptions())
+                    .load(url)
+                    .error(R.drawable.news_headlines_1)
+                    .into(view)
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+
+        @SuppressLint("SimpleDateFormat")
+        @BindingAdapter("timeAgoFormat")
+        @JvmStatic
+        fun convertToTimeAgoFormat(textView: TextView, time: String) {
+            try {
+                val timeAgo = formatTimeAgo(time)
+                textView.text = timeAgo
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
