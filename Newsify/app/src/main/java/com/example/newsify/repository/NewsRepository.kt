a4393b646c367b36a86c9d3c807b776d7fc0c3b2
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-03-02 21:39:24 +0530

    Added news repository class

--- Newsify/app/src/main/java/com/example/newsify/repository/NewsRepository.kt
+++ Newsify/app/src/main/java/com/example/newsify/repository/NewsRepository.kt
@@ -1,4 +1,15 @@
 package com.example.newsify.repository
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.example.newsify.retrofit.response.Article
+import org.json.JSONException
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class NewsRepository {
+
+
 }
